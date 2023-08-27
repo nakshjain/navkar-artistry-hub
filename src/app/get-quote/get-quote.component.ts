863031babc0a63b1600879496b5b458a12745975
Nakshatra Jain <nakshatrajain246@gmail.com>
2023-08-28 00:17:27 +0530

    Get Quote Email

--- src/app/get-quote/get-quote.component.ts
+++ src/app/get-quote/get-quote.component.ts
@@ -12,7 +12,7 @@ import {Product} from "../types/products.types";
 export class GetQuoteComponent implements OnInit{
   yourInfo='Your Information'
   yourBudget='Your Budget'
-  caption='Unlock the Beauty of Artistry: Discover Your Canvas\'s Worth with a Custom Quote!'
+  caption=`Unlock the Beauty of Artistry: Discover Your Canvas's Worth with a Custom Quote!`
 
   title='Get Quote'
   product: Product={
@@ -21,7 +21,7 @@ export class GetQuoteComponent implements OnInit{
     about:'',
     imageUrl:'',
     category:'',
-    available: '0'
+    available: '0',
   };
 
   backgroundColor='#FFF0F5'
@@ -38,9 +38,33 @@ export class GetQuoteComponent implements OnInit{
     communicationMethod:'',
     budget:''
   };
+  greeting='Hello,'
+  aboutUser=''
+  contactUser=''
+  budgetUser=''
+  endingStatement= 'I look forward to your response and learning more about the offerings you provide. Thank you for your assistance.'
+  enquiryMessage=''
+  emailSubject=''
+  emailBody=''
+  emailAddress='navkarartistryhub@gmail.com'
+
+  openEmailClient(): void {
+    if (this.formData) {
+      console.log()
+      const mailtoLink = `mailto:${this.emailAddress}?subject=${this.emailSubject}&body=${this.emailBody}`;
+      window.location.href = mailtoLink;
+    }
+  }
 
   submitForm() {
-    console.log('Form data submitted:', this.formData);
+    this.aboutUser=`I hope this message finds you well. My name is ` + this.formData.title+` `+this.formData.firstName+` `+this.formData.lastName+  ` . I am interested in your services and would like to inquire about the pricing details for`+this.product.id+`.`
+    this.contactUser=`Please reach out to me at `+this.formData.email+` or `+this.formData.contactNumber+` to provide the information. I prefer to be contacted via `+this.formData.communicationMethod+`.`
+    this.budgetUser=`Additionally, I am curious about the budget required for this service. Could you please provide me with an estimate of the budget range? My budget for this project is approximately `+this.formData.budget+`.`
+    this.enquiryMessage=this.greeting+'\n'+this.aboutUser+'\n'+this.contactUser+'\n'+this.budgetUser+'\n'+this.endingStatement
+
+    this.emailSubject = encodeURIComponent(`Request for Information - Product Pricing`);
+    this.emailBody = encodeURIComponent(this.enquiryMessage);
+    this.openEmailClient()
   }
 
   getProductById(id: string){
